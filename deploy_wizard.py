a9ea2b5fdc4517d280a7b85ae8878c83e7b78f47
Neel Desai <neelbhaveshdesai@gmail.com>
2025-04-25 16:08:52 -0700

    integration for ec2 added

---
+++ deploy_wizard.py
@@ -0,0 +1,29 @@
+from aws_helper.ec2_deploy_wizard import EC2Deploy
+if __name__=="__main__":
+    print("Where would you like to deploy your code?")
+    print("1. AWS EC2")
+    print("2. Azure VM")
+    print("3. Print Kubernetes")
+    print("4. AWS SageMaker")
+    print("5. Azure ML")
+    input = input("Please enter the number corresponding to your choice: ")
+    if input == "1":
+        print("You have selected AWS EC2.")
+        # Add your AWS EC2 deployment code here
+        ec2_deploy = EC2Deploy()
+        ec2_deploy.deploy_ec2()
+
+    elif input == "2":
+        print("You have selected Azure VM.")
+        # Add your Azure VM deployment code here
+    elif input == "3":
+        print("You have selected Print Kubernetes.")
+        # Add your Print Kubernetes deployment code here
+    elif input == "4":
+        print("You have selected AWS SageMaker.")
+        # Add your AWS SageMaker deployment code here
+    elif input == "5":
+        print("You have selected Azure ML.")
+        # Add your Azure ML deployment code here
+    else:
+        print("Invalid selection. Please try again.")
